6592de83c882b7d536ddb8d87c639a08755061d8
Marcelo Calsing <marcelocalsing@gmail.com>
2022-08-18 17:06:37 -0300

    Trocando pokedex de diretorio

--- pokedex/src/PokemonDetails.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-class PokemonDetails extends React.Component {
-    render() {
-      return <section>PokemonDetails</section>
-    }
-}
-export default PokemonDetails;
